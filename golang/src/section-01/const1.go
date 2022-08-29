8b1a6976dc35516871150577f80f8d3a104b40f0
HanKyungMin <ehalshbest@playwith.co.kr>
2022-08-29 17:29:34 +0900

    study-golang first

---
+++ golang/src/section-01/const1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// const 사용 초기화, 한번 선언 후 값 변경 금지, 고정 된 값 관리 용
+
+	const a string = "Test1"
+	const b = "Test2"
+	const c int32 = 10 * 10
+	// const d = getHeight()
+	const e = 35.6
+	const f = false
+
+	/*
+		에러 발생
+		const g string
+		g = "Test3"
+	*/
+
+	fmt.Println("a :", a, "b :", b, "c :", c, "e :", e, "f :", f)
+}
